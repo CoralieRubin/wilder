341a3dfb9df4e23f143054a6ee210dd3b3d647ae
coralie Rubin <rubin.coralie@gmail.com>
2019-10-02 12:12:18 +0200

    Enfin

---
+++ wilder.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Anna'));
+console.log(chalk.yellow('Sebastien'));
+console.log(chalk.white('Camille'));
+console.log(chalk.grey('Helene'));
